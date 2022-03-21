c16f95ce44fe40a8ee22ea2a6940be57b87c8371
chaincatalyst <soliditymaster@outlook.com>
2022-03-21 23:30:18 +0100

    FormatUpdate

--- contracts/AlphaSwapFactory.sol
+++ contracts/AlphaSwapFactory.sol
@@ -2,19 +2,23 @@
 pragma solidity ^0.8.7;
 
 import "./AlphaSwapPair.sol";
-import "hardhat/console.sol";
+// import "hardhat/console.sol";
 
 contract AlphaSwapFactory {
-
-
     event PairCreated(address indexed _addressOfPair);
 
     mapping(address => mapping(address => address)) public pairAddress;
     AlphaSwapPair[] public alphaSwapArray;
 
-
-    function checkIfPairExists(address _token0, address _token1) public view returns (bool, address){
-        if (pairAddress[_token0][_token1] == address(0) && pairAddress[_token1][_token0] == address(0)) {
+    function checkIfPairExists(address _token0, address _token1)
+        public
+        view
+        returns (bool, address)
+    {
+        if (
+            pairAddress[_token0][_token1] == address(0) &&
+            pairAddress[_token1][_token0] == address(0)
+        ) {
             address addrOfPair = pairAddress[_token0][_token1];
             return (false, addrOfPair);
         } else {
@@ -23,10 +27,16 @@ contract AlphaSwapFactory {
         }
     }
 
-    function createPair(address _token0, address _token1) external returns(address) {
-        require(_token0 != address(0) && _token1 != address(0), "toknes can't equal to the zero address");
-        
-        (bool exists,) = checkIfPairExists(_token0, _token1);
+    function createPair(address _token0, address _token1)
+        external
+        returns (address)
+    {
+        require(
+            _token0 != address(0) && _token1 != address(0),
+            "toknes can't equal to the zero address"
+        );
+
+        (bool exists, ) = checkIfPairExists(_token0, _token1);
         require(!exists, "this pair already exists");
         AlphaSwapPair pair = new AlphaSwapPair();
         alphaSwapArray.push(pair);
